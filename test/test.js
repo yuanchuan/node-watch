079c61235781234031063e8f65614743ac8c2425
Timo Tijhof <krinklemail@gmail.com>
2018-12-30 14:04:49 -0800

    test: make the tests a bit faster and easier to debug
    * Some tests assert inside the watch callback, and some tests
      assert outside the watch callback.
      Asserting from the outside, I think, makes the test easier to
      understand when there is a failure. Asserting from inside the
      watch callback, it can cause a confusing error if it gets called
      multiple times (due to it calling 'done()' multiple times,
      which Mocha does not allow).
    
    * Use assert.equal(a, b) instead of assert(a === b) so that
      Mocha can report in the command-line what "actual" value is.
    
    * Use on('ready') inside the tests to avoid the delay of 200ms.
    
    * Use on('close', done) inside beforeEach().
    
    * Use delay:0 in most tests, except those that test the
      de-duplication behaviour. And, reduce the time distance between
      modifications from 300ms to 100ms so that the tests run much faster.
    
      It seems that distances shorter than 100ms might not be detected
      by fs.FSWatcher, so I did not make it smaller than that.
      This is based on testing on macOS with Node 10, and testing on
      Travis CI.
    
    * Set `--slow 500` parameter.
      The default in Mocha is 2000ms for error timeout,
      and 75ms for "slow" (marked with red color).
      The timeout is big enough, but the red color is too common
      on node-watch, I think. Increase from 75ms to 500ms so that we
      do not have red colors unless there is a problem.
    
    The tests now take about 4 seconds (previously, 29s).

--- test/test.js
+++ test/test.js
@@ -7,8 +7,12 @@ var watcher;
 
 beforeEach(function(done) {
   tree = Tree();
-  if (watcher && !watcher.isClosed()) watcher.close();
-  setTimeout(done, 500);
+  if (watcher && !watcher.isClosed()) {
+    watcher.on('close', done);
+    watcher.close();
+  } else {
+    done();
+  }
 });
 
 after(function() {
@@ -17,103 +21,7 @@ after(function() {
   }
 });
 
-
-describe('watch for files', function() {
-  it('should watch a single file and keep watching', function(done) {
-    this.timeout(3000);
-    var times = 1;
-    var file = 'home/a/file1';
-    var fpath = tree.getPath(file);
-    watcher = watch(fpath, function(evt, name) {
-      assert.equal(fpath, name)
-      if (times++ >= 3) {
-        done();
-      }
-    });
-    tree.modify(file, 100);
-    tree.modify(file, 500);
-    tree.modify(file, 900);
-  });
-
-  it('should watch files inside a directory', function(done) {
-    var fpath = tree.getPath('home/a');
-    var stack = [
-      tree.getPath('home/a/file1'),
-      tree.getPath('home/a/file2')
-    ];
-    watcher = watch(fpath, function(evt, name) {
-      stack.splice(stack.indexOf(name), 1);
-      if (!stack.length) done();
-    });
-
-    tree.modify('home/a/file1', 200);
-    tree.modify('home/a/file2', 300);
-  });
-
-  it('should ignore duplicate changes', function(done) {
-    var file = 'home/a/file2';
-    var fpath = tree.getPath(file);
-    var times = 0;
-    watcher = watch(fpath, function(evt, name) {
-      if (fpath === name) times++;
-      setTimeout(function() {
-        assert(times === 1)
-        done();
-      }, 200);
-    });
-    tree.modify(file, 100);
-    tree.modify(file, 120);
-    tree.modify(file, 150);
-  });
-
-});
-
-
-describe('watch for directoies', function() {
-
-  it('should watch directories inside a directory', function(done) {
-    var home = tree.getPath('home');
-    var dir = tree.getPath('home/c');
-
-    watcher = watch(home, { recursive: true }, function(evt, name) {
-      assert.equal(dir, name);
-      done();
-    });
-
-    tree.remove('home/c', 300);
-  });
-
-  it('should watch new created directories', function(done) {
-    var home = tree.getPath('home');
-    watcher = watch(home, { recursive: true }, function(evt, name) {
-      if (name === tree.getPath('home/new/file1')) {
-        done();
-      }
-    });
-    tree.newFile('home/new/file1', 200);
-    tree.modify('home/new/file1', 500);
-  });
-});
-
-describe('events', function() {
-  it('should identify `remove` event', function(done) {
-    var file = 'home/a/file1';
-    var fpath = tree.getPath(file);
-    watcher = watch(fpath, function(evt, name) {
-      if (evt === 'remove' && name === fpath) done();
-    });
-    tree.remove(file, 100);
-  });
-
-  it('should identify `update` event', function(done) {
-    var file = 'home/a/file1';
-    var fpath = tree.getPath(file);
-    watcher = watch(fpath, function(evt, name) {
-      if (evt === 'update' && name === fpath) done();
-    });
-    tree.modify(file, 100);
-  });
-
+describe('process events', function() {
   it('should emit `close` event', function(done) {
     var file = 'home/a/file1';
     var fpath = tree.getPath(file);
@@ -140,6 +48,117 @@ describe('events', function() {
       done();
     });
   });
+});
+
+describe('watch for files', function() {
+  it('should watch a single file and keep watching', function(done) {
+    var times = 1;
+    var file = 'home/a/file1';
+    var fpath = tree.getPath(file);
+    watcher = watch(fpath, { delay: 0 }, function(evt, name) {
+      assert.equal(fpath, name)
+      if (times++ >= 3) {
+        done();
+      }
+    });
+    watcher.on('ready', function() {
+      tree.modify(file);
+      tree.modify(file, 100);
+      tree.modify(file, 200);
+    });
+  });
+
+  it('should watch files inside a directory', function(done) {
+    var fpath = tree.getPath('home/a');
+    var stack = [
+      tree.getPath('home/a/file1'),
+      tree.getPath('home/a/file2')
+    ];
+    watcher = watch(fpath, { delay: 0 }, function(evt, name) {
+      stack.splice(stack.indexOf(name), 1);
+      if (!stack.length) done();
+    });
+
+    watcher.on('ready', function() {
+      tree.modify('home/a/file1');
+      tree.modify('home/a/file2', 100);
+    });
+  });
+
+  it('should ignore duplicate changes', function(done) {
+    var file = 'home/a/file2';
+    var fpath = tree.getPath(file);
+    var times = 0;
+    watcher = watch(fpath, { delay: 200 }, function(evt, name) {
+      if (fpath === name) times++;
+    });
+    watcher.on('ready', function() {
+      tree.modify(file);
+      tree.modify(file, 100);
+      tree.modify(file, 150);
+
+      setTimeout(function() {
+        assert.equal(times, 1)
+        done();
+      }, 250);
+    });
+  });
+});
+
+describe('watch for directories', function() {
+  it('should watch directories inside a directory', function(done) {
+    var home = tree.getPath('home');
+    var dir = tree.getPath('home/c');
+
+    watcher = watch(home, { delay: 0, recursive: true }, function(evt, name) {
+      assert.equal(dir, name);
+      done();
+    });
+    watcher.on('ready', function() {
+      tree.remove('home/c');
+    });
+  });
+
+  it('should watch new created directories', function(done) {
+    var home = tree.getPath('home');
+    watcher = watch(home, { delay: 0, recursive: true }, function(evt, name) {
+      if (name === tree.getPath('home/new/file1')) {
+        done();
+      }
+    });
+    watcher.on('ready', function() {
+      // newFile() will create the 'new/' directory and the 'new/file1' file,
+      // but, only the creation of the directory is observed.
+      // Because of that, there will only be one event for file1, when it
+      // is modified, not when it is created.
+      tree.newFile('home/new/file1');
+      tree.modify('home/new/file1', 100);
+    });
+  });
+});
+
+describe('file events', function() {
+  it('should identify `remove` event', function(done) {
+    var file = 'home/a/file1';
+    var fpath = tree.getPath(file);
+    watcher = watch(fpath, function(evt, name) {
+      if (evt === 'remove' && name === fpath) done();
+    });
+    watcher.on('ready', function() {
+      tree.remove(file);
+    });
+  });
+
+  it('should identify `update` event', function(done) {
+    var file = 'home/a/file1';
+    var fpath = tree.getPath(file);
+    watcher = watch(fpath, function(evt, name) {
+      if (evt === 'update' && name === fpath) done();
+    });
+    watcher.on('ready', function() {
+      tree.modify(file);
+    });
+  });
 
   it('should report `update` on new files', function(done) {
     var dir = tree.getPath('home/a');
@@ -148,9 +167,10 @@ describe('events', function() {
     watcher = watch(dir, function(evt, name) {
       if (evt === 'update' && name === fpath) done();
     });
-    tree.newFile(file);
+    watcher.on('ready', function() {
+      tree.newFile(file);
+    });
   });
-
 });
 
 describe('options', function() {
@@ -162,7 +182,9 @@ describe('options', function() {
         assert.equal(file, name);
         done();
       });
-      tree.modify('home/bb/file1', 300);
+      watcher.on('ready', function() {
+        tree.modify('home/bb/file1');
+      });
     });
   });
 
@@ -181,10 +203,12 @@ describe('options', function() {
       var file = 'home/a/file1';
       var fpath = tree.getPath(file);
       watcher = watch(dir, 'utf8', function(evt, name) {
-        assert(name.toString() === fpath);
+        assert.equal(name.toString(), fpath);
         done();
       });
-      tree.modify(file, 200);
+      watcher.on('ready', function() {
+        tree.modify(file);
+      });
     });
 
     it('should support buffer encoding', function(done) {
@@ -193,10 +217,12 @@ describe('options', function() {
       var fpath = tree.getPath(file);
       watcher = watch(dir, 'buffer', function(evt, name) {
         assert(Buffer.isBuffer(name), 'not a Buffer')
-        assert(name.toString() === fpath);
+        assert.equal(name.toString(), fpath);
         done();
       });
-      tree.modify(file, 200);
+      watcher.on('ready', function() {
+        tree.modify(file);
+      });
     });
 
     it('should support base64 encoding', function(done) {
@@ -204,13 +230,16 @@ describe('options', function() {
       var file = 'home/a/file1';
       var fpath = tree.getPath(file);
       watcher = watch(dir, 'base64', function(evt, name) {
-        assert(
-          name === Buffer.from(fpath).toString('base64'),
+        assert.equal(
+          name,
+          Buffer.from(fpath).toString('base64'),
           'wrong base64 encoding'
         );
         done();
       });
-      tree.modify(file, 200);
+      watcher.on('ready', function() {
+        tree.modify(file);
+      });
     });
 
     it('should support hex encoding', function(done) {
@@ -218,44 +247,49 @@ describe('options', function() {
       var file = 'home/a/file1';
       var fpath = tree.getPath(file);
       watcher = watch(dir, 'hex', function(evt, name) {
-        assert(
-          name === Buffer.from(fpath).toString('hex'),
+        assert.equal(
+          name,
+          Buffer.from(fpath).toString('hex'),
           'wrong hex encoding'
         );
         done();
       });
-      tree.modify(file, 200);
+      watcher.on('ready', function() {
+        tree.modify(file);
+      });
     });
   });
 
   describe('filter', function() {
     it('should only watch filtered directories', function(done) {
-      var shouldModify = true;
-      var shouldNotModify = false;
+      var matchRegularDir = false;
+      var matchIgnoredDir = false;
 
-      var option = {
+      var options = {
+        delay: 0,
         recursive: true,
         filter: function(name) {
           return !/deep_node_modules/.test(name);
         }
       };
 
-      watcher = watch(tree.getPath('home'), option, function(evt, name) {
+      watcher = watch(tree.getPath('home'), options, function(evt, name) {
         if (/deep_node_modules/.test(name)) {
-          shouldNotModify = true;
+          matchIgnoredDir = true;
         } else {
-          shouldModify = false;
+          matchRegularDir = true;
         }
       });
+      watcher.on('ready', function() {
+        tree.modify('home/b/file1');
+        tree.modify('home/deep_node_modules/ma/file1');
 
-      tree.modify('home/b/file1', 200);
-      tree.modify('home/deep_node_modules/ma/file1', 500);
-
-      setTimeout(function() {
-        assert(!shouldModify, 'watch failed');
-        assert(!shouldNotModify, 'fail to ingore path `deep_node_modules`');
-        done();
-      }, 900);
+        setTimeout(function() {
+          assert(matchRegularDir, 'watch failed to detect regular file');
+          assert(!matchIgnoredDir, 'fail to ignore path `deep_node_modules`');
+          done();
+        }, 100);
+      });
     });
 
     it('should only report filtered files', function(done) {
@@ -264,6 +298,7 @@ describe('options', function() {
       var file2 = 'home/bb/file2';
 
       var options = {
+        delay: 0,
         recursive: true,
         filter: function(name) {
           return !/file1/.test(name);
@@ -271,16 +306,23 @@ describe('options', function() {
       }
 
       var times = 0;
+      var matchIgnoredFile = false;
       watcher = watch(dir, options, function(evt, name) {
         times++;
-        if (name === tree.getPath(file2)) {
-          assert(times, 1, 'home/bb/file1 should be ignored.');
-          done();
+        if (name === tree.getPath(file1)) {
+          matchIgnoredFile = true;
         }
       });
+      watcher.on('ready', function() {
+        tree.modify(file1);
+        tree.modify(file2);
 
-      tree.modify(file1, 200);
-      tree.modify(file2, 400);
+        setTimeout(function() {
+          assert(times, 1, 'report file2');
+          assert(!matchIgnoredFile, 'home/bb/file1 should be ignored');
+          done();
+        }, 100);
+      });
     });
 
     it('should be able to filter with regexp', function(done) {
@@ -289,21 +331,29 @@ describe('options', function() {
       var file2 = 'home/bb/file2';
 
       var options = {
+        delay: 0,
         recursive: true,
         filter:  /file2/
       }
 
       var times = 0;
+      var matchIgnoredFile = false;
       watcher = watch(dir, options, function(evt, name) {
         times++;
-        if (name === tree.getPath(file2)) {
-          assert(times, 1, 'home/bb/file1 should be ignored.');
-          done();
+        if (name === tree.getPath(file1)) {
+          matchIgnoredFile = true;
         }
       });
+      watcher.on('ready', function() {
+        tree.modify(file1);
+        tree.modify(file2);
 
-      tree.modify(file1, 200);
-      tree.modify(file2, 400);
+        setTimeout(function() {
+          assert(times, 1, 'report file2');
+          assert(!matchIgnoredFile, 'home/bb/file1 should be ignored');
+          done();
+        }, 100);
+      });
     });
   });
 
@@ -312,22 +362,19 @@ describe('options', function() {
       var dir = tree.getPath('home/a');
       var file = 'home/a/file1';
       var start;
-      watcher = watch(dir, { delay: 1000 }, function(evt, name) {
-        assert(Date.now() - start > 1000, 'delay not working');
+      watcher = watch(dir, { delay: 300 }, function(evt, name) {
+        assert(Date.now() - start > 300, 'delay not working');
         done();
       });
-      setTimeout(function() {
+      watcher.on('ready', function() {
         tree.modify(file);
         start = Date.now();
-      }, 200);
+      });
     });
   });
-
 });
 
-
 describe('parameters', function() {
-
   it('should throw error on non-existed file', function(done) {
     var somedir = tree.getPath('home/somedir');
     try {
@@ -339,11 +386,13 @@ describe('parameters', function() {
 
   it('should accept filename as Buffer', function(done) {
     var fpath = tree.getPath('home/a/file1');
-    watcher = watch(Buffer.from(fpath), function(evt, name) {
-      assert(name === fpath);
+    watcher = watch(Buffer.from(fpath), { delay: 0 }, function(evt, name) {
+      assert.equal(name, fpath);
       done();
     });
-    tree.modify('home/a/file1', 100);
+    watcher.on('ready', function() {
+      tree.modify('home/a/file1');
+    });
   });
 
   it('should compose array of files or directories', function(done) {
@@ -355,13 +404,14 @@ describe('parameters', function() {
     ];
 
     var times = 0;
-    watcher = watch(fpaths, function(evt, name) {
+    watcher = watch(fpaths, { delay: 0 }, function(evt, name) {
       if (fpaths.indexOf(name) !== -1) times++;
       if (times === 2) done();  // calling done more than twice causes mocha test to fail
     });
 
-    tree.modify(file1, 100);
-    tree.modify(file2, 150);
+    // FIXME: composeWatcher doesn't support "ready" event
+    tree.modify(file1);
+    tree.modify(file2);
   });
 
   it('should filter duplicate events for composed watcher', function(done) {
@@ -377,81 +427,87 @@ describe('parameters', function() {
 
     var changed = [];
 
-    watcher = watch(fpaths, { recursive: true }, function(evt, name) {
+    watcher = watch(fpaths, { delay: 0, recursive: true }, function(evt, name) {
       changed.push(name);
     });
 
-    tree.modify(file3, 100);
-    tree.newFile(newFile, 200);
+    tree.modify(file3);
+    tree.newFile(newFile);
 
     setTimeout(function() {
-      assert.strictEqual(changed.length, 2, 'should log extactly 2 events');
+      assert.strictEqual(changed.length, 2, 'should log exactly 2 events');
       assert(~changed.indexOf(tree.getPath(file3)), 'should include ' + file3);
       assert(~changed.indexOf(tree.getPath(newFile)), 'should include ' + newFile);
       done();
-    }, 600);
+    }, 300);
   });
 
 });
 
 
 describe('watcher object', function() {
-
   it('should using watcher object to watch', function(done) {
     var dir = tree.getPath('home/a');
     var file = 'home/a/file1';
     var fpath = tree.getPath(file);
 
-    watcher = watch(dir);
-    watcher.on('change', function(evt, name) {
-      assert(evt === 'update');
-      assert(name === fpath);
-      done();
+    watcher = watch(dir, { delay: 0 });
+    watcher.on('ready', function() {
+      watcher.on('change', function(evt, name) {
+        assert.equal(evt, 'update');
+        assert.equal(name, fpath);
+        done();
+      });
+      tree.modify(file);
     });
-
-    tree.modify(file);
   });
 
   it('should close a watcher using .close()', function(done) {
     var dir = tree.getPath('home/a');
     var file = 'home/a/file1';
     var times = 0;
-    watcher = watch(dir);
+    watcher = watch(dir, { delay: 0 });
     watcher.on('change', function(evt, name) {
       times++;
     });
-    watcher.close();
+    watcher.on('ready', function() {
 
-    tree.modify(file);
-    tree.modify(file, 300);
-    setTimeout(function() {
-      assert(watcher.isClosed(), 'watcher should be closed');
-      assert(times === 0, 'failed to close the watcher');
-      done();
-    }, 400);
+      watcher.close();
+
+      tree.modify(file);
+      tree.modify(file, 100);
+
+      setTimeout(function() {
+        assert(watcher.isClosed(), 'watcher should be closed');
+        assert.equal(times, 0, 'failed to close the watcher');
+        done();
+      }, 150);
+    });
   });
 
   it('Do not emit after close', function(done) {
     var dir = tree.getPath('home/a');
     var file = 'home/a/file1';
     var times = 0;
-    watcher = watch(dir);
+    watcher = watch(dir, { delay: 0 });
     watcher.on('change', function(evt, name) {
       times++;
     });
+    watcher.on('ready', function() {
 
-    watcher.close();
+      watcher.close();
 
-    var timer = setInterval(function() {
-      tree.modify(file);
+      var timer = setInterval(function() {
+        tree.modify(file);
+      });
+
+      setTimeout(function() {
+        clearInterval(timer);
+        assert(watcher.isClosed(), 'watcher should be closed');
+        assert.equal(times, 0, 'failed to close the watcher');
+        done();
+      }, 100);
     });
-
-    setTimeout(function() {
-      clearInterval(timer);
-      assert(watcher.isClosed(), 'watcher should be closed');
-      assert(times === 0, 'failed to close the watcher');
-      done();
-    }, 400);
   });
 
 });
