721571965d159026ad26a0244cae3c50a90b3c88
yuanchuan <yuanchuan23@gmail.com>
2020-10-05 22:35:29 +0800

    Update type definition

--- lib/watch.d.ts
+++ lib/watch.d.ts
@@ -1,63 +1,75 @@
 import { FSWatcher } from 'fs';
 
 /**
- * Watch for changes on `filename`, where filename is either a file or a directory. The second argument is optional.
+ *  Watch for changes on `filename`, where filename is either a file or a directory.
+ *  The second argument is optional.
  *
- * - If `options` is provided as a string, it specifies the encoding. Otherwise `options` should be passed as an object.
+ *  If `options` is provided as a string, it specifies the encoding.
+ *  Otherwise `options` should be passed as an object.
  *
- * - The listener callback gets two arguments `(eventType, filename)`. `eventType` is either `update` or `remove`, and
- * `filename` is the name of the file which triggered the event.
+ *  The listener callback gets two arguments, `(eventType, filePath)`,
+ *  which is the same with `fs.watch`.
+ *  `eventType` is either `update` or `remove`,
+ *  `filePath` is the name of the file which triggered the event.
  *
  * @param {Filename} filename File or directory to watch.
- * @param {WatchOptions|string} options
- * @param {Function} listener
+ * @param {Options|string} options
+ * @param {Function} callback
  */
-declare function watch(filename: Filename, listener : (eventType: 'update' | 'remove', filename: string) => any) : ImprovedFSWatcher;
-declare function watch(filename: Filename, options : string, listener : (eventType: 'update' | 'remove', filename: string) => any) : ImprovedFSWatcher;
-declare function watch(filename: Filename, options : WatchOptions, listener : (eventType: 'update' | 'remove', filename: string) => any) : ImprovedFSWatcher;
+declare function watch(pathName: PathName): Watcher;
+declare function watch(pathName: PathName, options: Options) : Watcher;
+declare function watch(pathName: PathName, callback: Callback): Watcher;
+declare function watch(pathName: PathName, options: Options, callback: Callback): Watcher;
 
-interface FilenameArray extends Array<FilenameArray | string> {}
+type EventType = 'update' | 'remove';
+type Callback = (eventType ?: EventType, filePath ?: string) => any;
+type PathName = string | Array<string>;
+type FilterReturn = boolean | symbol;
 
-type Filename = string | FilenameArray;
+type Options = {
+  /**
+   * Indicates whether the process should continue to run
+   * as long as files are being watched.
+   * @default true
+   */
+  persistent ?: boolean;
 
-type WatchOptions = {
-    /**
-     * Indicates whether the process should continue to run as long as files are being watched.
-     * @default true
-     */
-    persistent ?: boolean;
+  /**
+   * Indicates whether all subdirectories should be watched.
+   * @default false
+   */
+  recursive ?: boolean;
 
-    /**
-     * Indicates whether all subdirectories should be watched, or only the current directory. This applies when a
-     * directory is specified, and only on supported platforms.
-     *
-     * @default false
-     */
-    recursive ?: boolean;
+  /**
+   * Specifies the character encoding to be used for the filename
+   * passed to the listener.
+   * @default 'utf8'
+   */
+  encoding ?: string;
 
-    /**
-     * Specifies the character encoding to be used for the filename passed to the listener.
-     * @default 'utf8'
-     */
-    encoding ?: string;
+  /**
+   * Only files which pass this filter (when it returns `true`)
+   * will be sent to the listener.
+   */
+  filter ?: RegExp | ((file: string, skip: symbol) => FilterReturn);
 
-    /**
-     * Only files which pass this filter (when it returns `true`) will be sent to the listener.
-     */
-    filter ?: RegExp | ((file: string) => boolean);
-
-    /**
-     * Delay time of the callback function.
-     * @default 200
-     */
-    delay ?: number;
+  /**
+   * Delay time of the callback function.
+   * @default 200
+   */
+  delay ?: number;
 };
 
-declare interface ImprovedFSWatcher extends FSWatcher {
-    /**
-     * Returns `true` if the watcher has been closed.
-     */
-    isClosed(): boolean;
+declare interface Watcher extends FSWatcher {
+  /**
+   * Returns `true` if the watcher has been closed.
+   */
+  isClosed(): boolean;
+
+  /**
+   * Returns all watched paths.
+   */
+  getWatchedPaths(): Array<string>;
 }
 
 export default watch;
